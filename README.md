a4f1d34cfc4d6d02408112bdcdca530a09beb1ca
dav-cho <chodavid@gmail.com>
2021-04-09 17:29:27 -0400

    basic js functionality

--- README.md
+++ README.md
@@ -35,7 +35,7 @@ Me after project week.
 | README.md            |    H     |     2 hrs      |     1 hrs     |    1 hrs    |
 | Wireframing          |    M     |     2 hrs      |     2 hrs     |    2 hrs    |
 | Initial HTML & CSS   |    M     |     2 hrs      |     3 hrs     |    3 hrs    |
-| Basic JS             |    H     |     2 hrs      |     x hrs     |    x hrs    |
+| Basic JS             |    H     |     3 hrs      |     2 hrs     |    2 hrs    |
 | User Text Input      |    H     |     3 hrs      |     x hrs     |    x hrs    |
 | Typing Prompt        |    H     |     6 hrs      |     x hrs     |    x hrs    |
 | API Research         |    L     |     1 hrs      |     x hrs     |    x hrs    |
@@ -43,7 +43,7 @@ Me after project week.
 | Typing Stats Cards   |    L     |     4 hrs      |     x hrs     |    x hrs    |
 | Modals               |    L     |     3 hrs      |     x hrs     |    x hrs    |
 | CSS                  |    H     |     5 hrs      |     x hrs     |    x hrs    |
-| Totals               |    x     |     35 hrs     |     5 hrs     |    5 hrs    |
+| Totals               |    x     |     35 hrs     |     10 hrs    |    10 hrs   |
 
 ## Code Snippet
 
--- src/app.js
+++ src/app.js
@@ -1,18 +1,65 @@
+const prompt = document.getElementById('prompt');
+const currentPrompt = prompt.childNodes;
 const userInput = document.getElementById('user-input');
+const wpmDisplay = document.getElementById('wpm');
+const timerDisplay = document.getElementById('timer');
+const mistakesDisplay = document.getElementById('mistakes');
 
-function handleInput(e) {
-  console.log('~ e', e)
-  console.log('~ userInput', userInput.value)
-}
-
-userInput.addEventListener('input', handleInput);
-// userInput.addEventListener('change', handleInput);
+let difficulty = 'easy';
 
 async function getLorem() {
-  const res = await fetch('src/lorem-ipsum.json');
-  console.log('~ res', res)
-  const dummy = await res.json();
-  console.log('~ dummy', dummy)
+  try {
+    const res = await fetch('src/dummy.json');
+    const dummy = await res.json();
+    console.log('SUCCESS', res);
+
+    return dummy[difficulty][Math.floor(Math.random() * 20)]
+      .split(' ')
+      .slice(0, 5); // sliced length to 5 for testing
+  } catch (err) {
+    console.log('ERROR', err);
+  }
 }
 
-getLorem();
+// render prompt;
+async function renderPrompt() {
+  const words = await getLorem();
+
+  words.forEach(word => {
+    const wordTile = document.createElement('div');
+    wordTile.classList.add('word-tile');
+    wordTile.id = word;
+    wordTile.innerText = word;
+    prompt.append(wordTile);
+  });
+}
+
+function handleFocus() {
+  if (!currentPrompt.length) renderPrompt();
+  // start timer
+}
+
+function handleEnter(e) {
+  if (e.key === 'Enter' || e.key === ' ') {
+    // check for matching word
+    currentPrompt.forEach(div => {
+      if (div.innerText === userInput.value.trim()) {
+        div.remove();
+        userInput.value = null;
+      }
+    });
+    // if prompt is empty, render new prompt
+    if (!currentPrompt.length) renderPrompt();
+  }
+}
+
+// use for letter highlighting
+function handleInput() {
+  console.log('~ currentPrompt', currentPrompt);
+}
+
+userInput.addEventListener('keydown', handleEnter);
+userInput.addEventListener('input', handleInput);
+userInput.addEventListener('focus', handleFocus);
+// const startStop = document.getElementById('start-stop');
+// startStop.addEventListener('click', changePrompt);
